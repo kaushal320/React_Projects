e917c45512ba8bc3ae06ec4087aa863bf82c3714
kaushal <127113014+kaushal320@users.noreply.github.com>
2025-04-25 12:37:45 +0545

    Made a simple meal api

---
+++ Meals-API-Project/meal-api/src/Components/Meal.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "../style.css";
+const Meal = () => {
+  const [items, setitems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+      .then((res) => {
+        // console.log(res.data);
+        setitems(res.data.meals);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const itemslist = items.map(({ strMeal, strMealThumb, idMeal }) => {
+    return (
+      <section className="card">
+        <img src={strMealThumb} />
+        <section className="content">
+          <p>{strMeal}</p>
+          <p>#{idMeal}</p>
+        </section>
+      </section>
+    );
+  });
+
+  return <div className="items-container">{itemslist}</div>;
+};
+
+export default Meal;
